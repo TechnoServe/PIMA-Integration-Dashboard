1d28b7e51b1d15f8f2fce18a44ad1a248345b3f9
bahatiphill <gur79tub@gmail.com>
2022-06-21 12:10:56 +0200

    FT: application reading environment variables from .env file

--- PIMA_Dashboard/settings.py
+++ PIMA_Dashboard/settings.py
@@ -10,23 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Initialize Environment varibales
+env =  environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sgp8&*znw+uomw7bocm&gz5($tfd1b)l)p4&^jmf7aat*_6mcm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 

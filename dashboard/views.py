9a847b3ab874c11e070b59a313f7284de7cd4fdd
bahatiphill <gur79tub@gmail.com>
2022-06-22 13:01:56 +0200

    [FEAT]: Added popup information on markers

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,25 @@
 import folium
 from django.shortcuts import render
-from dashboard.models import Farm
+from dashboard.models import Farm, Farmer
 
 # Create your views here.
 
 def dashboard_map(request):
     
-    coordinates = list(Farm.objects.values_list('latitude', 'langitude'))
+    #coordinates = list(Farm.objects.values_list('latitude', 'langitude'))
+    farms = list(Farm.objects.values())
 
     country = [-1.9437057, 29.8805778]  #Rwanda
     map =  folium.Map(location=country, zoom_start=8)
 
-    for item in coordinates:
-        folium.Marker(item).add_to(map)
+    for item in farms:
+        cord = [item.get('latitude'), item.get('langitude')]
+        owner = Farmer.objects.get(id=item.get('owner_id')).firstname
+        trees = item.get('number_of_trees')
+        UPI = item.get('UPI')
+        #pop_info = f'Trees:{trees} Trees:{trees}'
+        pop_info = f'<i>Owner</i>:<b>{owner}</b><br><i>Trees</i>:<b>{trees}</b><br><i>UPI</i>:<b>{UPI}</b>'
+        folium.Marker(cord, popup=pop_info, tooltip = "Click me!").add_to(map)
     
     folium.raster_layers.TileLayer('Stamen Terrain').add_to(map)
     folium.raster_layers.TileLayer('Stamen Toner').add_to(map)

704fbf90e3f36e921c4caa017d6e225af1ec302d
bahatiphill <gur79tub@gmail.com>
2022-07-07 12:50:45 +0200

    [FIX] reduced time taken to run background task from ~150secs to ~15secs

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -21,27 +21,25 @@ def getToken():
 @shared_task(bind=True)
 def getObservations(self):
     
-    Observation_Ids = list()
     Observations = list()
     holder = dict()
 
     TOKEN = getToken()
     sf = Salesforce(instance_url=f'https://{SALESFORCE_INSTANCE}', session_id=TOKEN)
-    Obs_result = sf.query_all("SELECT Id FROM Observation__c WHERE Date__c >= 2022-06-01")
+
+    Obs_result = sf.query_all(
+        "SELECT Id,Date__c,Project_Name__c,Observation_Location__Latitude__s,Observation_Location__Longitude__s,Trainer__c FROM Observation__c WHERE Date__c >= 2022-06-01"
+    )
+    
     Obs_records =  Obs_result.get('records')
 
     for row in Obs_records:
-        Observation_Ids.append(row.get('Id'))
-    
-    for id_ in Observation_Ids:
-
-        current_obs = sf.Observation__c.get(id_)
-        holder['Id'] = id_
-        holder['Project_Name__c'] = current_obs.get('Project_Name__c')
-        holder['Trainer__c'] = current_obs['Trainer__c'] #trainer = sf.Contact.get('0031o00001Zxz7pAAB')
-        holder['Observation_Location__Latitude__s'] =  current_obs.get('Observation_Location__Latitude__s')
-        holder['Observation_Location__Longitude__s'] = current_obs.get('Observation_Location__Longitude__s')
-        holder['Date__c'] = current_obs.get('Date__c')
+        holder['Id'] = row.get('Id')
+        holder['Project_Name__c'] = row.get('Project_Name__c')
+        holder['Trainer__c'] = row.get('Trainer__c') #trainer = sf.Contact.get('0031o00001Zxz7pAAB')
+        holder['Observation_Location__Latitude__s'] =  row.get('Observation_Location__Latitude__s')
+        holder['Observation_Location__Longitude__s'] = row.get('Observation_Location__Longitude__s')
+        holder['Date__c'] = row.get('Date__c')
         Observations.append(holder)
         holder = {}
     

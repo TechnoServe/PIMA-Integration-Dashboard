11cc2643b2ac95bae3b925cfc097d2e5550e45c4
bahatiphill <gur79tub@gmail.com>
2022-08-17 15:10:45 +0200

    [FEATURE] deleting user

--- PIMA_Dashboard/settings.py
+++ PIMA_Dashboard/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import environ
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -188,3 +189,12 @@ AUTH_USER_MODEL = 'accounts.User'
 #LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 #LOGIN_URL = 'login'
+
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
--- accounts/views.py
+++ accounts/views.py
@@ -38,7 +38,7 @@ def user_logout(request):
     logout(request)
     return redirect('login')
 
-@user_passes_test(lambda u: u.is_superuser)
+@user_passes_test(lambda user: user.is_superuser)
 def create_user_account(request):
     context = {}
 
@@ -57,10 +57,30 @@ def create_user_account(request):
         context['form'] = form
     return render(request, 'registration/create_user.html', context=context )
 
-@user_passes_test(lambda user_: user_.is_superuser)
+@user_passes_test(lambda user: user.is_superuser)
 def manage_users(request):
     user_object = get_user_model()
-    users = user_object.objects.all()
+    users = user_object.objects.filter(is_active=True)
     context = {'users': users}
     return render(request, 'user_dash.html', context)
 
+
+@user_passes_test(lambda user: user.is_superuser)
+def delete_user(request, id):
+
+    user_object = get_user_model()
+
+    try:
+        u = user_object.objects.get(id=id)
+        u.is_active = False
+        u.save()
+        messages.success(request, "User is deleted successfully")
+        return redirect('manage_users')
+
+    except user_object.DoesNotExist:
+        messages.error(request, "User doesnot exist")    
+        return redirect('manage_users')
+
+    except Exception as e: 
+        messages.error(request, "There are some errors")    
+        return redirect('manage_users')
